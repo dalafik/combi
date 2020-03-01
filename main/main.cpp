70865f09f8a2a4481626904082c9f0f2bbb63dd6
Denis Smirnov <denis.smirnov.ru@gmail.com>
2020-03-01 18:35:40 +0300

    lab-1.1 added run results

--- main/main.cpp
+++ main/main.cpp
@@ -4,9 +4,18 @@
 #include <iostream>
 #include <vector>
 
+// n = 5 with output, t = 0.85s
+// n = 6 with output, t = 6.48s
+
+// n = 10 without output, t = 0.04s
+// n = 12 without output, t = 3.3s
+// n = 13 without output, t = 44.0s
+
 int main()
 {
-	std::vector<int> permut = { 7, 1, 12, 5, 9, 20, 15 };
+	//std::vector<int> permut = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+	//std::vector<int> permut = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	std::vector<int> permut = { 0, 1, 2, 3, 4, 5, 6};
 	std::sort(permut.begin(), permut.end());
 	std::unique(permut.begin(), permut.end());
 
