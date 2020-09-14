07a53a11b1ea37260e452cdf0134dd7f368ce517
Denis Smirnov <denis.smirnov.ru@gmail.com>
2020-09-14 12:09:58 +0300

    master branch cleanup

--- main/main.cpp
+++ main/main.cpp
@@ -1,20 +1,10 @@
 #include "common/benchmark.hpp"
 #include "common/stdext.hpp"
-#include "JohnsonTrotter.hpp"
 #include <algorithm>
 #include <iostream>
 #include <vector>
 
-// n = 10 without output, t = 0.51s
-// n = 12 without output, t = 51.77s
-
 int main()
 {
-	Benchmark([&]() {
-		JohnsonTrotter(12, [](auto permut) {
-			// std::cout << permut << std::endl;
-		});
-	});
-
 	return EXIT_SUCCESS;
 }
