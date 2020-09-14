64bcc8a785e62510565447b05d683eea66562e96
Denis Smirnov <denis.smirnov.ru@gmail.com>
2020-09-14 13:18:07 +0300

    lab-1.5 done

--- main/main.cpp
+++ main/main.cpp
@@ -3,8 +3,20 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include "kPermutations.hpp"
+
+// n == 10, k == 5, t == 0s
+// n == 20, k == 5, t == 0.03s
+// n == 50, k == 5, t == 3.2s
+// n == 70, k == 5, t == 17.1s
 
 int main()
 {
+	Benchmark([&]() {
+		kPermutations(10, 5, [](auto& permut) {
+			//std::cout << permut << std::endl;
+		});
+	});
+
 	return EXIT_SUCCESS;
 }
