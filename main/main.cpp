c084d8a190ba85792a74ece0dea4df56c8a41f76
Denis Smirnov <denis.smirnov.ru@gmail.com>
2020-09-16 08:59:46 +0300

    lab-2.1 done

--- main/main.cpp
+++ main/main.cpp
@@ -3,8 +3,37 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include "AdjacencyMatrix.hpp"
+#include "FindBridgesAndArticulationPoints.hpp"
 
 int main()
 {
+	size_t nVertices;
+	size_t nEdges;
+	std::vector<Edge> edges;
+
+	std::cin >> nVertices >> nEdges;
+
+	for (size_t i = 0; i < nEdges; ++i)
+	{
+		size_t v1, v2;
+		std::cin >> v1 >> v2;
+		edges.push_back(std::make_pair(v1 - 1, v2 - 1));
+	}
+
+	const auto graph = createAdjacencyMatrixFromEdges(nVertices, edges);
+	const auto [bridges, points] = FindBridgesAndArticulationPoints(graph);
+
+	for (auto& point : points)
+	{
+		std::cout << point + 1 << ' ';
+	}
+	std::cout << std::endl;
+
+	for (auto& [v1, v2] : bridges)
+	{
+		std::cout << v1 + 1 << ' ' << v2 + 1 << std::endl;
+	}
+
 	return EXIT_SUCCESS;
 }
