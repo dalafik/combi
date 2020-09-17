97ae243af534d0a61ea012ed31469b98d3572e9b
Denis Smirnov <denis.smirnov.ru@gmail.com>
2020-09-17 16:10:58 +0300

    lab-3.3 gauss

---
+++ test/FindDeterminant.test.cpp
@@ -0,0 +1,52 @@
+#include <initializer_list>
+#include "../main/FindDeterminant.hpp"
+#include "catch.hpp"
+
+TEST_CASE("FindDeterminant")
+{
+	SECTION("sample 1x1")
+	{
+		Matrix matrix = {
+			{ 4 },
+		};
+
+		const Rational determinant = FindDeterminant(matrix);
+		REQUIRE(determinant == 4);
+	}
+
+	SECTION("sample 2x2")
+	{
+		Matrix matrix = {
+			{ 4, 6 },
+			{ 3, 8 },
+		};
+
+		const Rational determinant = FindDeterminant(matrix);
+		REQUIRE(determinant == 14);
+	}
+
+	SECTION("sample 3x3")
+	{
+		Matrix matrix = {
+			{6, 1, 1},
+			{4, -2, 5},
+			{2, 8, 7},
+		};
+
+		const Rational determinant = FindDeterminant(matrix);
+		REQUIRE(determinant == -306);
+	}
+
+	SECTION("sample 4x4")
+	{
+		Matrix matrix = {
+			{ 4, 7, 2, 3 },
+			{ 1, 3, 1, 2 },
+			{ 2, 5, 3, 4 },
+			{ 1, 4, 2, 3 },
+		};
+
+		const Rational determinant = FindDeterminant(matrix);
+		REQUIRE(determinant == -3);
+	}
+}
